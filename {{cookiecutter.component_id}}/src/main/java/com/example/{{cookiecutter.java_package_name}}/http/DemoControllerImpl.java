8e38fce6f163daba67a2efbecd98bd77553e4eb9
elton <eltonmmoreira@gmail.com>
2021-04-19 23:00:58 -0300

    Initial commit

---
+++ {{cookiecutter.component_id}}/src/main/java/com/example/{{cookiecutter.java_package_name}}/http/DemoControllerImpl.java
@@ -0,0 +1,14 @@
+package com.example.{{cookiecutter.java_package_name}}.http;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoControllerImpl implements DemoController {
+
+  @Override
+  public ResponseEntity<String> hello(String param) {
+    return ResponseEntity.ok("Hello " + param);
+  }
