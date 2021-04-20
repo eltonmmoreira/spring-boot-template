511b2b2eb84f8d708e81e716947a8939327dc6cd
elton <eltonmmoreira@gmail.com>
2021-04-19 23:10:42 -0300

    fix class test

--- {{cookiecutter.component_id}}/src/test/java/com/example/{{cookiecutter.java_package_name}}/ApplicationTest.java
+++ {{cookiecutter.component_id}}/src/test/java/com/example/{{cookiecutter.java_package_name}}/ApplicationTest.java
@@ -1,15 +1,13 @@
 package com.example.{{cookiecutter.java_package_name}};
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTest {
+class TemplateApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
+
 }
